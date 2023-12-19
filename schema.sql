200cef1ddd65923717c0996cfd38589bdb7d2ee3
nikloynes <nl1676@nyu.edu>
2023-12-19 21:58:16 +0000

    first version of database schema

---
+++ schema.sql
@@ -0,0 +1,57 @@
+CREATE TABLE journeys (
+    journey_id TEXT PRIMARY KEY,
+    search_id TEXT,
+    n_legs INTEGER,
+    cabin_baggage INTEGER,
+    checked_baggage INTEGER,
+    class_ TEXT,
+    FOREIGN KEY(search_id) REFERENCES flight_searches(search_id)
+);
+
+CREATE TABLE legs (
+    leg_id INTEGER PRIMARY KEY,
+    journey_id INTEGER,
+    departure_time TEXT,
+    arrival_time TEXT,
+    departure_airport TEXT,
+    arrival_airport TEXT,
+    airline TEXT,
+    FOREIGN KEY(journey_id) REFERENCES journeys(journey_id)
+);
+
+CREATE TABLE prices (
+    price_id INTEGER PRIMARY KEY,
+    journey_id TEXT
+    price REAL,
+    currency TEXT,
+    created_at TIMESTAMP,
+    FOREIGN KEY(journey_id) REFERENCES journeys(journey_id)
+);
+
+CREATE TABLE flight_searches (
+    search_id TEXT PRIMARY KEY,
+    journey_type TEXT,
+    origin TEXT,
+    destination TEXT,
+    leave_date TEXT,
+    return_date TEXT,
+    flex INTEGER
+);
+
+CREATE TABLE searches_journeys_prices (
+    search_id INTEGER,
+    journey_id INTEGER,
+    price_id INTEGER,
+    PRIMARY KEY(search_id, journey_id, price_id),
+    FOREIGN KEY(search_id) REFERENCES flight_searches(search_id),
+    FOREIGN KEY(journey_id) REFERENCES journeys(journey_id),
+    FOREIGN KEY(price_id) REFERENCES prices(price_id)
+);
+
+CREATE TABLE journeys_prices (
+    journey_id INTEGER,
+    price_id INTEGER,
+    PRIMARY KEY(journey_id, price_id),
+    FOREIGN KEY(journey_id) REFERENCES journeys(journey_id),
+    FOREIGN KEY(price_id) REFERENCES prices(price_id)
+);
